f82675752617b325fd139a6e100fc485bb24a89f
IvanPLebedev <ivan.p.lebedev@yandex.ru>
2020-05-29 16:19:14 +0400

    commit 3 add func check()

--- sudoku.py
+++ sudoku.py
@@ -22,7 +22,34 @@ class Sudoku:
                 if check_list(li) == False:
                     return False
         return True
+    
+    def solve(self) -> bool:
+        coord = find_zero_in_matr(self.matrix)
+        # если вся матрица заполнена возвращаем true
+        if coord == (-1,-1):
+            return True
+        # пробуем подставить значения и проверяем
+        for i in range(1, 10, 1):
+            self.matrix[coord[0]][coord[1]] = i
+            if self.check():
+                res = self.solve()
+                if res:
+                    return res
+        # если ни одно значение не подходит возвращаем false   
+        self.matrix[coord[0]][coord[1]] = 0    
+        return False       
 
+def find_zero_in_matr(matr):
+    for i in range(len(matr)):
+        for j in range(len(matr[i])):
+            if matr[i][j] == 0:
+                return (i, j)
+    return (-1, -1)
+
+def print_2d_matr(matr):
+    for i in range(len(matr)):
+        print(matr[i])
+    print('\n\r')
         
 def check_list(li) -> bool:
     x = remove_in_list(li, 0)
@@ -36,7 +63,8 @@ def remove_in_list(li, elem):
 
 
 if __name__ == "__main__":
-    matrix = [
+    print('test1')
+    matrix1 = [
         [0,0,5,0,0,0,6,0,0],
         [6,0,0,0,1,0,0,0,3],
         [0,0,4,5,0,9,1,0,0],
@@ -47,5 +75,21 @@ if __name__ == "__main__":
         [7,0,0,0,2,0,0,0,6],
         [0,0,3,0,0,0,9,0,0],
         ]
-    sudoku = Sudoku(matrix)
-    print(sudoku.check())
+    sudoku1 = Sudoku(matrix1)
+    print(sudoku1.solve())
+    print_2d_matr(sudoku1.matrix)
+    print('test2')
+    matrix2 = [
+        [5,2,0,0,1,0,0,4,3],
+        [0,0,0,0,0,0,0,0,0],
+        [0,3,1,0,4,0,9,8,0],
+        [0,0,0,4,2,1,0,0,0],
+        [0,0,4,0,0,0,6,0,0],
+        [0,0,0,9,5,6,0,0,0],
+        [0,7,5,0,8,0,1,3,0],
+        [0,0,0,0,0,0,0,0,0],
+        [8,1,0,0,3,0,0,7,2],
+        ]
+    sudoku2 = Sudoku(matrix2)
+    print(sudoku2.solve())
+    print_2d_matr(sudoku2.matrix)
